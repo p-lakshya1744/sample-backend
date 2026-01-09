b445f091a3ec1f81f5fd254763190aa3eaddb8dd
Lakshya Patel <157837793+p-lakshya1744@users.noreply.github.com>
2026-01-09 14:49:03 +0530

    Initialize Flask app with home and health routes

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "success",
+        "message": "Backend is running on Render 🚀"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"health": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
